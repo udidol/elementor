6688f1e66fb0e1d77cab4cb6d5a3e2ede29c82fa
Nevo Golan <nevog@elementor.com>
2021-03-25 01:07:10 +0200

    wip

--- modules/comments/assets/js/my.js
+++ modules/comments/assets/js/my.js
@@ -26,12 +26,17 @@ function Comment( props ) {
 			.then( ( data ) => setChildren( ( prev ) => [ ...prev, data ] ) );
 	};
 
+	const date = new Date( props.date );
+
 	return (
 		<div className="comment-modal" id={`comment-modal-${ props.id }`}>
 			<div>
 				<div style={{ display: 'flex', alignItems: 'center' }}>
 					<img src={props.author_avatar_urls[ 48 ]} style={{ height: '30px', 	borderRadius: '300px' }}/>
 					<span style={{ marginLeft: '10px' }}> {props.author_name} </span>
+				</div>
+				<div>
+					{ date.toLocaleString() }
 				</div>
 				<div dangerouslySetInnerHTML={{ __html: props.content.rendered }} />
 
@@ -149,6 +154,8 @@ jQuery( () => {
 				const iconWrapper = document.createElement( 'div' );
 				const icon = document.createElement( 'div' );
 
+				icon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="27" height="38" fill="none"><g filter="url(#filter0_f)"><ellipse cx="14" cy="33" fill="#000" fill-opacity=".12" rx="6" ry="3"/></g><mask id="a" width="25" height="33" x="1" y="1.375" fill="#000" maskUnits="userSpaceOnUse"><path fill="#fff" d="M1 1.375h25v33H1z"/><path fill-rule="evenodd" d="M19.905 23.825c2.787-2.161 4.595-5.645 4.595-9.575 0-6.558-5.037-11.875-11.25-11.875S2 7.692 2 14.25c0 3.93 1.808 7.414 4.595 9.575 2.124 1.69 5.12 4.358 5.794 8.658.067.43.426.763.861.763s.794-.333.861-.763c.673-4.3 3.67-6.968 5.794-8.658z" clip-rule="evenodd"/></mask><path fill="#4AB7F4" fill-rule="evenodd" d="M19.905 23.825c2.787-2.161 4.595-5.645 4.595-9.575 0-6.558-5.037-11.875-11.25-11.875S2 7.692 2 14.25c0 3.93 1.808 7.414 4.595 9.575 2.124 1.69 5.12 4.358 5.794 8.658.067.43.426.763.861.763s.794-.333.861-.763c.673-4.3 3.67-6.968 5.794-8.658z" clip-rule="evenodd"/><path fill="url(#paint0_linear)" d="M19.905 23.825l-.613-.79-.009.007.622.783zm-13.31 0l.622-.783-.01-.007-.612.79zm5.794 8.658l-.988.155.988-.155zm1.722 0l.988.155-.988-.155zm6.407-7.868c3.03-2.35 4.982-6.126 4.982-10.365h-2c0 3.62-1.665 6.813-4.207 8.785l1.225 1.58zM25.5 14.25c0-7.059-5.434-12.875-12.25-12.875v2c5.61 0 10.25 4.817 10.25 10.875h2zM13.25 1.375C6.434 1.375 1 7.191 1 14.25h2C3 8.192 7.64 3.375 13.25 3.375v-2zM1 14.25c0 4.24 1.952 8.015 4.982 10.365l1.226-1.58C4.665 21.062 3 17.87 3 14.25H1zm12.377 18.078c-.737-4.703-4.013-7.578-6.16-9.286l-1.245 1.566c2.102 1.671 4.819 4.133 5.429 8.03l1.976-.31zm-.127-.082c.046 0 .081.02.1.036a.08.08 0 01.027.046l-1.976.31c.137.873.88 1.608 1.849 1.608v-2zm-.127.082a.08.08 0 01.027-.046.154.154 0 01.1-.036v2c.97 0 1.712-.735 1.85-1.608l-1.977-.31zm6.16-9.286c-2.147 1.708-5.423 4.583-6.16 9.286l1.976.31c.61-3.897 3.327-6.359 5.429-8.03l-1.245-1.566z" mask="url(#a)"/><defs><linearGradient id="paint0_linear" x1="13.25" x2="13.25" y1="2.375" y2="33.246" gradientUnits="userSpaceOnUse"><stop stop-color="#fff"/><stop offset="1" stop-color="#fff" stop-opacity=".35"/></linearGradient><filter id="filter0_f" width="16" height="10" x="6" y="28" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape"/><feGaussianBlur result="effect1_foregroundBlur" stdDeviation="1"/></filter></defs></svg>';
+
 				iconWrapper.classList.add( 'comment-icon-wrapper' );
 				icon.classList.add( 'comment-icon' );
 
@@ -196,6 +203,8 @@ jQuery( () => {
 
 		iconWrapper.classList.add( 'comment-icon-wrapper' );
 		icon.classList.add( 'comment-icon' );
+
+		icon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="27" height="38" fill="none"><g filter="url(#filter0_f)"><ellipse cx="14" cy="33" fill="#000" fill-opacity=".12" rx="6" ry="3"/></g><mask id="a" width="25" height="33" x="1" y="1.375" fill="#000" maskUnits="userSpaceOnUse"><path fill="#fff" d="M1 1.375h25v33H1z"/><path fill-rule="evenodd" d="M19.905 23.825c2.787-2.161 4.595-5.645 4.595-9.575 0-6.558-5.037-11.875-11.25-11.875S2 7.692 2 14.25c0 3.93 1.808 7.414 4.595 9.575 2.124 1.69 5.12 4.358 5.794 8.658.067.43.426.763.861.763s.794-.333.861-.763c.673-4.3 3.67-6.968 5.794-8.658z" clip-rule="evenodd"/></mask><path fill="#4AB7F4" fill-rule="evenodd" d="M19.905 23.825c2.787-2.161 4.595-5.645 4.595-9.575 0-6.558-5.037-11.875-11.25-11.875S2 7.692 2 14.25c0 3.93 1.808 7.414 4.595 9.575 2.124 1.69 5.12 4.358 5.794 8.658.067.43.426.763.861.763s.794-.333.861-.763c.673-4.3 3.67-6.968 5.794-8.658z" clip-rule="evenodd"/><path fill="url(#paint0_linear)" d="M19.905 23.825l-.613-.79-.009.007.622.783zm-13.31 0l.622-.783-.01-.007-.612.79zm5.794 8.658l-.988.155.988-.155zm1.722 0l.988.155-.988-.155zm6.407-7.868c3.03-2.35 4.982-6.126 4.982-10.365h-2c0 3.62-1.665 6.813-4.207 8.785l1.225 1.58zM25.5 14.25c0-7.059-5.434-12.875-12.25-12.875v2c5.61 0 10.25 4.817 10.25 10.875h2zM13.25 1.375C6.434 1.375 1 7.191 1 14.25h2C3 8.192 7.64 3.375 13.25 3.375v-2zM1 14.25c0 4.24 1.952 8.015 4.982 10.365l1.226-1.58C4.665 21.062 3 17.87 3 14.25H1zm12.377 18.078c-.737-4.703-4.013-7.578-6.16-9.286l-1.245 1.566c2.102 1.671 4.819 4.133 5.429 8.03l1.976-.31zm-.127-.082c.046 0 .081.02.1.036a.08.08 0 01.027.046l-1.976.31c.137.873.88 1.608 1.849 1.608v-2zm-.127.082a.08.08 0 01.027-.046.154.154 0 01.1-.036v2c.97 0 1.712-.735 1.85-1.608l-1.977-.31zm6.16-9.286c-2.147 1.708-5.423 4.583-6.16 9.286l1.976.31c.61-3.897 3.327-6.359 5.429-8.03l-1.245-1.566z" mask="url(#a)"/><defs><linearGradient id="paint0_linear" x1="13.25" x2="13.25" y1="2.375" y2="33.246" gradientUnits="userSpaceOnUse"><stop stop-color="#fff"/><stop offset="1" stop-color="#fff" stop-opacity=".35"/></linearGradient><filter id="filter0_f" width="16" height="10" x="6" y="28" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feBlend in="SourceGraphic" in2="BackgroundImageFix" result="shape"/><feGaussianBlur result="effect1_foregroundBlur" stdDeviation="1"/></filter></defs></svg>';
 
 		iconWrapper.appendChild( icon );
 
