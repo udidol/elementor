39424ad6f5c6116dd8f9d99a167e1603486e7f1e
udidol <udi.dollberg@gmail.com>
2021-03-24 22:44:07 +0200

    adjust running comments navigator in editor and frontend

--- modules/comments/assets/js/module.js
+++ modules/comments/assets/js/module.js
@@ -2,12 +2,24 @@ import CommentsNavigator from './comments-navigator';
 
 export default class extends elementorModules.Module {
 	onInit() {
+		// Load in Editor
 		elementorCommon.elements.$window.on( 'elementor:loaded', () => {
 			elementor.on( 'panel:init', () => {
-				this.commentsNavigator = new CommentsNavigator();
-
-				this.commentsNavigator.initModal();
+				this.initNavigator();
 			} );
 		} );
+
+		// Load in Frontend
+		elementorCommon.elements.$window.on( 'elementor/frontend/init', () => {
+			if ( ! elementorFrontend.isEditMode() ) {
+				this.initNavigator();
+			}
+		} );
+	}
+
+	initNavigator() {
+		this.commentsNavigator = new CommentsNavigator();
+
+		this.commentsNavigator.initModal();
 	}
 }
