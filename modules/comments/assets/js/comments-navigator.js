258129ba610d130fdff0594c46cfe7be17791f0b
udidol <udi.dollberg@gmail.com>
2021-03-24 23:50:59 +0200

    Added rendering for comment navigator templates

--- modules/comments/assets/js/comments-navigator.js
+++ modules/comments/assets/js/comments-navigator.js
@@ -30,20 +30,46 @@ export default class extends elementorModules.ViewModule {
 		} else {
 			this.elements.$adminBarToggle.on( 'click', () => this.toggleComments() );
 		}
+
 		this.elements.$closeButton.on( 'click', () => this.modal.hide() );
+	}
+
+	getModalHeader() {
+		return this.renderTemplate( '#tmpl-e-comments-header' );
+	}
+
+	getModalFooter() {
+		return this.renderTemplate( '#tmpl-e-comments-footer' );
+	}
+
+	getEmptyContent() {
+		return this.renderTemplate( '#tmpl-e-comments--empty' );
+	}
+
+	getCommentsContent() {
+		return this.renderTemplate( '#tmpl-e-comments', { comments: elementorCommon.config.comments } );
 	}
 
 	getModalContent() {
 		const headerContent = this.getModalHeader(),
-			container = jQuery( '<div/>', { class: '.e-comments-container' } );
+			footerContent = this.getModalFooter(),
+			container = jQuery( '<div/>', { id: 'e-comments-container' } );
 
-		container.html( headerContent );
+		let commentsContent;
 
-		return jQuery( '<div/>', { class: '.e-comments-container' } ).html( headerContent );
+		if ( elementorCommon.config.comments.length ) {
+			commentsContent = this.getCommentsContent();
+		} else {
+			commentsContent = this.getEmptyContent();
+		}
+
+		container.html( headerContent + commentsContent + footerContent );
+
+		return container;
 	}
 
-	getModalHeader() {
-		return Marionette.Renderer.render( Marionette.TemplateCache.get( '#tmpl-e-comments' ) );
+	renderTemplate( template, args = null ) {
+		return Marionette.Renderer.render( Marionette.TemplateCache.get( template ), args );
 	}
 
 	initModal() {
--- modules/comments/template.php
+++ modules/comments/template.php
@@ -7,12 +7,30 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 <script type="text/template" id="tmpl-e-comments">
+	<div id="e-comments__elements">
+		<# comments.forEach( ( comment ) => { #>
+			<div class="e-comments__comment-item">
+				<div class="e-comments__comment-item-id">{{{ comment.id }}}</div>
+				<div class="e-comments__comment_meta">
+					<span class="e-comments__comment-author">{{{ comment.author }}}</span>
+					<span class="e-comments__comment-timestamp">{{{ comment.timestamp }}}</span>
+					<p class="e-comments__comment-excerpt">{{ comment.content }}</p>
+					<p class="e-comments__num-replies">{{{ comment.numReplies }}}</p>
+				</div>
+			</div>
+		<#} ); #>
+	</div>
+</script>
+
+<script type="text/template" id="tmpl-e-comments-header">
 	<div id="e-comments__header">
 		<span class="e-comments-empty"></span>
 		<div id="e-comments__header__title"><?php echo __( 'Comments', 'elementor' ); ?></div>
 		<i id="e-comments__close" class="eicon-close"></i>
 	</div>
-	<div id="e-comments__elements"></div>
+</script>
+
+<script type="text/template" id="tmpl-e-comments-footer">
 	<div id="e-comments__footer">
 		<div id="e-comments__footer-icons">
 			<i id="e-comments__toggle-show" class="eicon-preview-medium"></i>
@@ -21,9 +39,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 		</div>
 		<a href="#" id="e-comments__add-comment">Add Comment</a>
 	</div>
-	<!--<div id="e-comments__footer-bottom">
-		<i class="eicon-ellipsis-h"></i>
-	</div>-->
 </script>
 
 <script type="text/template" id="tmpl-e-comments__elements">
@@ -39,8 +54,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 </script>
 
 <script type="text/template" id="tmpl-e-comments--empty">
-	<img alt="<?php echo __( 'Comment Icon', 'elementor' ); ?>" class="elementor-nerd-box-icon" src="<?php echo ELEMENTOR_URL . 'modules/comments/assets/images/comment.svg'; ?>" />
-	<div class="elementor-nerd-box-title"><?php echo __( 'Start Commenting!', 'elementor' ); ?></div>
-	<div class="elementor-nerd-box-message"><?php echo __( 'Give feedback, ask a question or just leave a note of appreciation. Click anywhere on the page to leave a comment.', 'elementor' ); ?></div>
-	<div class="elementor-nerd-box-message"><?php echo sprintf( __( 'You can also leave notes to self (only you can see) by clicking on the <img id="e-comments__unlock" src="%s" /> icon.', 'elementor' ), ELEMENTOR_URL . 'modules/comments/assets/images/unlock.svg' ); ?></div>
+	<div id="e-comments__empty">
+		<img alt="<?php echo __( 'Comment Icon', 'elementor' ); ?>" id="e-comments__no-comments-icon" src="<?php echo ELEMENTOR_URL . 'modules/comments/assets/images/comment.svg'; ?>" />
+		<div id="e-comments__no-comments-title"><?php echo __( 'Start Commenting!', 'elementor' ); ?></div>
+		<div id="e-comments__no-comments-message">
+			<p><?php echo __( 'Give feedback, ask a question or just leave a note of appreciation. Click anywhere on the page to leave a comment.', 'elementor' ); ?></p>
+			<p><?php echo sprintf( __( 'You can also leave notes to self (only you can see) by clicking on the <img id="e-comments__unlock" src="%s" /> icon.', 'elementor' ), ELEMENTOR_URL . 'modules/comments/assets/images/unlock.svg' ); ?></p>
+		</div>
+	</div>
 </script>
